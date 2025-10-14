9893e44ee15bf3801e8e1c5a06cc288c70e64e08
James Pelter <james.pelter@glitchprod.com>
2025-10-14 02:45:19 -0400

    More work on 1.21.10 porting - Mostly compiles now, just a few last mixin issues.

--- fabric/src/main/java/com/trainguy9512/locomotion/LocomotionFabric.java
+++ fabric/src/main/java/com/trainguy9512/locomotion/LocomotionFabric.java
@@ -4,8 +4,10 @@ import com.trainguy9512.locomotion.resource.LocomotionResources;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,15 +17,10 @@ import java.util.concurrent.Executor;
 public class LocomotionFabric implements ClientModInitializer {
     
     private void registerResourceReloader() {
-        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new IdentifiableResourceReloadListener() {
+        ResourceLoader.get(PackType.CLIENT_RESOURCES).registerReloader(LocomotionResources.RELOADER_IDENTIFIER, new PreparableReloadListener() {
             @Override
-            public @NotNull CompletableFuture<Void> reload(PreparationBarrier barrier, ResourceManager manager, Executor backgroundExecutor, Executor gameExecutor) {
-                return LocomotionResources.reload(barrier, manager, backgroundExecutor, gameExecutor);
-            }
-
-            @Override
-            public ResourceLocation getFabricId() {
-                return LocomotionResources.RELOADER_IDENTIFIER;
+            public @NotNull CompletableFuture<Void> reload(@NotNull SharedState sharedState, @NotNull Executor exectutor, @NotNull PreparationBarrier barrier, @NotNull Executor applyExectutor) {
+                return LocomotionResources.reload(barrier, sharedState.resourceManager(), exectutor, applyExectutor);
             }
         });
     }
