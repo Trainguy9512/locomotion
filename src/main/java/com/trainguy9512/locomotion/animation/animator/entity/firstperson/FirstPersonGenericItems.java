bb986ff3e9a55936d5fa4736af6be590d7a6ec28
James Pelter <james.pelter@glitchprod.com>
2025-12-08 16:27:13 -0600

    Single blocks now render again (without shading right now)

--- src/main/java/com/trainguy9512/locomotion/animation/animator/entity/firstperson/FirstPersonGenericItems.java
+++ src/main/java/com/trainguy9512/locomotion/animation/animator/entity/firstperson/FirstPersonGenericItems.java
@@ -7,12 +7,18 @@ import com.trainguy9512.locomotion.animation.pose.LocalSpacePose;
 import com.trainguy9512.locomotion.animation.pose.function.*;
 import com.trainguy9512.locomotion.animation.pose.function.cache.CachedPoseContainer;
 import com.trainguy9512.locomotion.render.ItemRenderType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 
 import java.util.*;
 import java.util.function.Predicate;
@@ -53,6 +59,12 @@ public class FirstPersonGenericItems {
             80)
             .setItemRenderType(ItemRenderType.MIRRORED_THIRD_PERSON_ITEM)
             .build());
+    public static final ResourceLocation BLOCK = register(LocomotionMain.makeResourceLocation("block"), GenericItemPoseDefinition.builder(
+                    FirstPersonAnimationSequences.HAND_GENERIC_ITEM_BLOCK_POSE,
+                    FirstPersonGenericItems::isBlockItem,
+                    80)
+            .setItemRenderType(ItemRenderType.BLOCK_STATE)
+            .build());
 
     private static final List<Item> ROD_ITEMS = List.of(
             Items.BONE,
@@ -86,6 +98,124 @@ public class FirstPersonGenericItems {
     private static boolean isArrowItem(ItemStack itemStack) {
         return itemStack.is(ItemTags.ARROWS);
     }
+
+    private static boolean isBlockItem(ItemStack itemStack) {
+        ResourceLocation identifier = BuiltInRegistries.ITEM.getKey(itemStack.getItem());
+        ResourceLocation modelLocation = ResourceLocation.fromNamespaceAndPath(identifier.getNamespace(), "models/item/" + identifier.getPath() + ".json");
+        return Minecraft.getInstance().getResourceManager().getResource(modelLocation).isEmpty();
+    }
+
+//    public static final List<Item> BLOCK_2D_OVERRIDE_ITEMS = List.of(
+//            Items.IRON_BARS,
+//            Items.GLASS_PANE,
+//            Items.WHITE_STAINED_GLASS_PANE,
+//            Items.ORANGE_STAINED_GLASS_PANE,
+//            Items.MAGENTA_STAINED_GLASS_PANE,
+//            Items.LIGHT_BLUE_STAINED_GLASS_PANE,
+//            Items.YELLOW_STAINED_GLASS_PANE,
+//            Items.LIME_STAINED_GLASS_PANE,
+//            Items.PINK_STAINED_GLASS_PANE,
+//            Items.GRAY_STAINED_GLASS_PANE,
+//            Items.LIGHT_GRAY_STAINED_GLASS_PANE,
+//            Items.CYAN_STAINED_GLASS_PANE,
+//            Items.PURPLE_STAINED_GLASS_PANE,
+//            Items.BLUE_STAINED_GLASS_PANE,
+//            Items.BROWN_STAINED_GLASS_PANE,
+//            Items.GREEN_STAINED_GLASS_PANE,
+//            Items.RED_STAINED_GLASS_PANE,
+//            Items.BLACK_STAINED_GLASS_PANE,
+//            Items.POINTED_DRIPSTONE,
+//            Items.SMALL_AMETHYST_BUD,
+//            Items.MEDIUM_AMETHYST_BUD,
+//            Items.LARGE_AMETHYST_BUD,
+//            Items.AMETHYST_CLUSTER,
+//            Items.RED_MUSHROOM,
+//            Items.BROWN_MUSHROOM,
+//            Items.DEAD_BUSH,
+//            Items.SHORT_GRASS,
+//            Items.TALL_GRASS,
+//            Items.FERN,
+//            Items.LARGE_FERN,
+//            Items.CRIMSON_FUNGUS,
+//            Items.WARPED_FUNGUS,
+//            Items.BAMBOO,
+//            Items.SUGAR_CANE,
+//            Items.SMALL_DRIPLEAF,
+//            Items.BIG_DRIPLEAF,
+//            Items.CRIMSON_ROOTS,
+//            Items.WARPED_ROOTS,
+//            Items.NETHER_SPROUTS,
+//            Items.WEEPING_VINES,
+//            Items.TWISTING_VINES,
+//            Items.VINE,
+//            Items.GLOW_BERRIES,
+//            Items.COCOA_BEANS,
+//            Items.LILY_PAD,
+//            Items.BRAIN_CORAL,
+//            Items.BUBBLE_CORAL,
+//            Items.HORN_CORAL,
+//            Items.FIRE_CORAL,
+//            Items.TUBE_CORAL,
+//            Items.DEAD_BRAIN_CORAL,
+//            Items.DEAD_BUBBLE_CORAL,
+//            Items.DEAD_HORN_CORAL,
+//            Items.DEAD_FIRE_CORAL,
+//            Items.DEAD_TUBE_CORAL,
+//            Items.BRAIN_CORAL_FAN,
+//            Items.BUBBLE_CORAL_FAN,
+//            Items.HORN_CORAL_FAN,
+//            Items.FIRE_CORAL_FAN,
+//            Items.TUBE_CORAL_FAN,
+//            Items.DEAD_BRAIN_CORAL_FAN,
+//            Items.DEAD_BUBBLE_CORAL_FAN,
+//            Items.DEAD_HORN_CORAL_FAN,
+//            Items.DEAD_FIRE_CORAL_FAN,
+//            Items.DEAD_TUBE_CORAL_FAN,
+//            Items.COBWEB,
+//            Items.LILAC,
+//            Items.PEONY,
+//            Items.ROSE_BUSH,
+//            Items.SUNFLOWER,
+//            Items.MANGROVE_PROPAGULE,
+//            Items.PINK_PETALS,
+//            Items.PITCHER_PLANT,
+//            Items.MELON_SEEDS,
+//            Items.PUMPKIN_SEEDS,
+//            Items.GLOW_LICHEN,
+//            Items.SCULK_VEIN,
+//            Items.NETHER_WART,
+//            Items.SWEET_BERRIES,
+//            Items.SEAGRASS,
+//            Items.KELP,
+//            Items.TORCH,
+//            Items.SOUL_TORCH,
+//            Items.REDSTONE_TORCH,
+//            Items.BELL,
+//            Items.LADDER,
+//            Items.LIGHTNING_ROD,
+//            Items.DECORATED_POT,
+//            Items.REDSTONE,
+//            Items.STRING,
+//            Items.TRIPWIRE_HOOK,
+//            Items.RAIL,
+//            Items.ACTIVATOR_RAIL,
+//            Items.DETECTOR_RAIL,
+//            Items.POWERED_RAIL,
+//            Items.FROGSPAWN,
+//            //? >= 1.21.4 {
+//            Items.PALE_HANGING_MOSS,
+//            Items.RESIN_CLUMP,
+//            //}
+//            //? >= 1.21.5 {
+//            Items.DRY_SHORT_GRASS,
+//            Items.DRY_TALL_GRASS,
+//            Items.BUSH,
+//            Items.FIREFLY_BUSH,
+//            Items.LEAF_LITTER,
+//            Items.CACTUS_FLOWER,
+//            Items.WILDFLOWERS
+//            //}
+//    );
 
     public record GenericItemPoseDefinition(
             ResourceLocation basePoseAnimationSequence,
